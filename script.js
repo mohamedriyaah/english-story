763c19b85173864933b3fee76c9e11d24e493a7a
mohamedriyaah <98955400+mohamedriyaah@users.noreply.github.com>
2022-04-22 09:57:53 -0700

    new
    hi my name is new

---
+++ script.js
@@ -0,0 +1,76 @@
+var SuperPlaceholder = function(options) {  
+    this.options = options;
+    this.element = options.element
+    this.placeholderIdx = 0;
+    this.charIdx = 0;
+    
+  
+    this.setPlaceholder = function() {
+        placeholder = options.placeholders[this.placeholderIdx];
+        var placeholderChunk = placeholder.substring(0, this.charIdx+1);
+        document.querySelector(this.element).setAttribute("placeholder", this.options.preText + " " + placeholderChunk)
+    };
+    
+    this.onTickReverse = function(afterReverse) {
+      if (this.charIdx === 0) {
+        afterReverse.bind(this)();
+        clearInterval(this.intervalId); 
+        this.init(); 
+      } else {
+        this.setPlaceholder();
+        this.charIdx--;
+      }
+    };
+    
+    this.goReverse = function() {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.onTickReverse.bind(this, function() {
+          this.charIdx = 0;
+          this.placeholderIdx++;
+          if (this.placeholderIdx === options.placeholders.length) {
+            // end of all placeholders reached
+            this.placeholderIdx = 0;
+          }
+        }), this.options.speed)
+    };
+    
+    this.onTick = function() {
+        var placeholder = options.placeholders[this.placeholderIdx];
+        if (this.charIdx === placeholder.length) {
+          // end of a placeholder sentence reached
+          setTimeout(this.goReverse.bind(this), this.options.stay);
+        }
+        
+        this.setPlaceholder();
+      
+        this.charIdx++;
+      }
+    
+    this.init = function() {
+      this.intervalId = setInterval(this.onTick.bind(this), this.options.speed);
+    }
+    
+    this.kill = function() {
+      clearInterval(this.intervalId); 
+    }
+  }
+  $(document).ready(function(){
+    $('.slider').bxSlider({
+      auto:true,
+      controls:false
+    }
+    );
+  });
+  window.onload = function() {
+    var sp = new SuperPlaceholder({
+      placeholders: ["kids story", "funny story", "romantic story", "horror story", "funny story", "english story", "adult stories", "moral story"],
+      preText: "Eg. ",
+      stay: 1000,
+      speed: 100,
+      element: '#placeholder'
+    });
+    sp.init();
+  }
+
+  
+  
